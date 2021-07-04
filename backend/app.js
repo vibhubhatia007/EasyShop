6c44d6681e919891f5da8a037128a5c239c9cc7f
Vibhu Bhatia <vibhubhatia0007@gmail.com>
2021-07-04 21:28:15 +0530

    password hashed,routes authorized and jwt added

--- backend/app.js
+++ backend/app.js
@@ -2,59 +2,38 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
-require('dotenv/config')
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
-
-//middle ware
-app.use (express.json());
-app.use(morgan('tiny'));
-
-
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
-
-const Product = mongoose.model('Product', productSchema);
-
-console.log(api);
-
 const PORT = process.env.PORT|| 5000;
 
-app.get('/products', (req, res) => {
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-    const produc = {
-        id : 1,
-        name: 'hair_dresser',
-        image: 'some_image',
-    }
 
-    res.send(produc);
-})
-
-app.post('/products', (req, res) => {
-
-    const product = new Product({
-       name: req.body.name,
-       image: req.body.image,
-       countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct=> {
-        res.status(201).json();
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            succcess: false
-        })
-    })
-
-    console.log(product);
-})
-
+//Database
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -67,7 +46,7 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err);
 })
 
-app.listen(5000, ()=> {
+//Server
+app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`);
 })
-
